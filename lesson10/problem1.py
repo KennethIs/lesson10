c9b320e1301faef92772821f15c05761e554cf4d
KennethIs <43756442+KennethIs@users.noreply.github.com>
2018-12-18 12:42:49 -0500

    Add files via upload

---
+++ lesson10/problem1.py
@@ -0,0 +1,12 @@
+print('-'*60)
+print('Life Decision Bot')
+print()
+
+food = input('Are you hungry? ')
+if food == 'Yes'or food =='yes' or food =='ye' or food =='Ye':
+	print('Then eat')
+	print('Make a sandwich or buy one if you want')
+else:
+	print('Then idk go do something')
+	print('How about you get a job?')
+print('-'*60)
---
+++ lesson10/problem3.py
@@ -0,0 +1,9 @@
+print('-'*60)
+print('Welcome to CLUB425, now leave. But before you leave, if you wanna go inside I gotta ask a series of complex questions.')
+print()
+answer = input('How old are you? ')
+if answer == '17':
+	print("Alright, nevermind bye.")
+else:
+	print('')
+age = input
